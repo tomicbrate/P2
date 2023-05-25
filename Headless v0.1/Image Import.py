b6f8141da0585803b7cf7e49a69f23e92d9131c9
tomicbrate <126027652+tomicbrate@users.noreply.github.com>
2023-05-25 21:36:08 +0200

    Update Image Import.py

--- Headless v0.1/Image Import.py
+++ Headless v0.1/Image Import.py
@@ -4,9 +4,9 @@ import cv2
 import os
 
 def open_image():
-  
-    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])    
- 
+
+    file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png")])
+
     if file_path:
 
         image = cv2.imread(file_path)
@@ -16,8 +16,11 @@ def open_image():
 
         file_dir, file_name = os.path.split(file_path)
         new_file_name = f"{first_name}_{last_name}{os.path.splitext(file_name)[1]}"
-        new_file_path = os.path.join(file_dir, new_file_name)
         
+        images_folder = os.path.join(os.path.dirname(__file__), "Images")
+        os.makedirs(images_folder, exist_ok=True)
+ 
+        new_file_path = os.path.join(images_folder, new_file_name)
         cv2.imwrite(new_file_path, image)
         
         print(f"Image saved as: {new_file_name}")
@@ -28,3 +31,4 @@ button = tk.Button(window, text="Open Image", command=open_image)
 button.pack()
 
 window.mainloop()
+
